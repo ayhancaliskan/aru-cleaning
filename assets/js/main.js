4fa779fe2c8f0eea44b4eb61bb7bbef4b88d78a0
Ayhan Caliskan <ayhan.caliskan@eezee-it.com>
2023-08-23 21:02:08 +0200

    add caroussel for section features and update colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,6 +83,43 @@ $(function () {
         time: 3000
     });
 
+    //===== features active
+    $('.features-active').slick({
+        dots: true,
+        speed: 800,
+        arrows: false,
+        centerMode: true,
+        centerPadding: "0",
+        slidesToShow: 3,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
 
     //===== testimonial active
 
